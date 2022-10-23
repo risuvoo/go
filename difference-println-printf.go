f8d340cd1a3692e0456554e7f0e3d444989ef6a5
meXuvo <rafiqulislamsuvobd@gmail.com>
2022-10-23 21:36:07 +0600

    [200~difference-println-printf.go~

---
+++ difference-println-printf.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world") //with new line
+	fmt.Println("hello world")
+
+	fmt.Printf("Hello world") //without new line and variable declaration formatting
+	fmt.Printf("Hello world")
+
+	fmt.Print("Hello world!!")
+}
