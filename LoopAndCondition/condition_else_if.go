bc7ce76d98ea382a0e22cff47edd9e69a84d492b
Rafiqul Islam Suvo <43400887+meXuvo@users.noreply.github.com>
2022-12-17 21:37:14 +0600

    else if condition

---
+++ LoopAndCondition/condition_else_if.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "3"
+	if name == "1" {
+		fmt.Println(1)
+	} else if name == "2" {
+		fmt.Println(2)
+	} else {
+		fmt.Println("nothing")
+	}
+}
